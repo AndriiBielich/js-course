946a802845c8466f32c46fc2c0b44e26e457c976
AndriiBielich <belichandrey89@gmail.com>
2025-08-01 16:35:18 +0300

    feat: Complete task 2

---
+++ homeworks/homework_1/task_2/scripts/main.js
@@ -0,0 +1,6 @@
+const firstName = 'Andrii';
+const middleName = 'Viktorovich';
+const lastName = 'Bielich';
+
+const fullName = firstName + ' ' + middleName + ' ' + lastName;
+console.log(fullName);
