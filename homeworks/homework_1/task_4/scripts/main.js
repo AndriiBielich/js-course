44522a105f6d77c99f60fef533a2d937b14c1bce
AndriiBielich <belichandrey89@gmail.com>
2025-08-01 16:41:43 +0300

    feat: Complete task 4

---
+++ homeworks/homework_1/task_4/scripts/main.js
@@ -0,0 +1,6 @@
+const firstName = prompt('Please enter your first name:');
+const middleName = prompt('Please enter your middle name:');
+const lastName = prompt('Please enter your last name:');
+
+const fullName = firstName + ' ' + middleName + ' ' + lastName;
+console.log(fullName);
