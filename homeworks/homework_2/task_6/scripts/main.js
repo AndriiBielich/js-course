4175a90f4c8a1bd5c6b52afc9686f9075ccfd5ff
AndriiBielich <belichandrey89@gmail.com>
2025-08-04 16:40:48 +0300

    feat: Complete Task 6

---
+++ homeworks/homework_2/task_6/scripts/main.js
@@ -0,0 +1,11 @@
+let x = 1;
+let response = x !== 0 ? 'Вірно' : 'Невірно';
+console.log(response);
+
+x = 0;
+response = x !== 0 ? 'Вірно' : 'Невірно';
+console.log(response);
+
+x = 3;
+response = x !== 0 ? 'Вірно' : 'Невірно';
+console.log(response);
