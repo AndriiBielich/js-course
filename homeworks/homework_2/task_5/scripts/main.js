638c4b64887ed3a99dcb97c37fdc27de96b8cc63
AndriiBielich <belichandrey89@gmail.com>
2025-08-04 16:35:47 +0300

    feat: Complete Task 5

---
+++ homeworks/homework_2/task_5/scripts/main.js
@@ -0,0 +1,44 @@
+const temperatures = [
+    {
+        date: '2025-08-04',
+        morningTemperature: 26,
+        middayTemperature: 32,
+        eveningTemperature: 24
+    },
+    {
+        date: '2025-08-05',
+        morningTemperature: 23,
+        middayTemperature: 29,
+        eveningTemperature: 25
+    },
+    {
+        date: '2025-08-06',
+        morningTemperature: 25,
+        middayTemperature: 34,
+        eveningTemperature: 24
+    },
+    {
+        date: '2025-08-07',
+        morningTemperature: 25,
+        middayTemperature: 33,
+        eveningTemperature: 28
+    },
+    {
+        date: '2025-08-08',
+        morningTemperature: 28,
+        middayTemperature: 36,
+        eveningTemperature: 30
+    },
+    {
+        date: '2025-08-09',
+        morningTemperature: 31,
+        middayTemperature: 37,
+        eveningTemperature: 32
+    },
+    {
+        date: '2025-08-10',
+        morningTemperature: 33,
+        middayTemperature: 39,
+        eveningTemperature: 29
+    }
+];
