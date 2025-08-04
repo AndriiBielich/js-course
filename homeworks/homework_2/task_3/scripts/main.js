16e56daad1bbafece061ca4b8e7efabbbebe7d64
AndriiBielich <belichandrey89@gmail.com>
2025-08-04 16:12:24 +0300

    feat: Complete task 3

---
+++ homeworks/homework_2/task_3/scripts/main.js
@@ -0,0 +1,43 @@
+const book1 = {
+    title: 'C++',
+    pageCount: 1897,
+    genre: 'Programming',
+    authors: [
+        {
+            name: 'Bjarne Stroustrup',
+            age: 74
+        },
+        {
+            name: 'Paul Deitel',
+            age: 70
+        }
+    ]
+};
+
+const book2 = {
+    title: 'Alice in Wonderland',
+    pageCount: 192,
+    genre: 'Fantasy',
+    authors: [
+        {
+            name: 'Lewis Caroll',
+            age: 64
+        },
+        {
+            name: 'John Tenniel',
+            age: 94
+        }
+    ]
+};
+
+const book3 = {
+    title: 'Storm of the century',
+    pageCount: 200,
+    genre: 'Fantasy',
+    authors: [
+        {
+            name: 'Stephen King',
+            age: 75
+        }
+    ]
+};
