350b33462ca06d9df902f77f021fd5caba49aec1
AndriiBielich <belichandrey89@gmail.com>
2025-08-04 16:26:54 +0300

    feat: Complete Task 4

---
+++ homeworks/homework_2/task_4/scripts/main.js
@@ -0,0 +1,65 @@
+const users = [
+    {
+        name: 'John',
+        username: 'john_doe',
+        password: '12345'
+    },
+    {
+        name: 'Alice',
+        username: 'a_brown',
+        password: 'qwerty123'
+    },
+    {
+        name: 'Hulk',
+        username: 'mr_muscle',
+        password: '123'
+    },
+    {
+        name: 'Bred',
+        username: 'mr_pit',
+        password: 'tgbyujmik,'
+    },
+    {
+        name: 'Kim',
+        username: 'kimmi',
+        password: 'lm89Yt5'
+    },
+    {
+        name: 'Aaron',
+        username: 'fbi_agent',
+        password: 's4e5c6r7e8t9'
+    },
+    {
+        name: 'Velma',
+        username: 'mystery',
+        password: 'doobie-doo'
+    },
+    {
+        name: 'Stan',
+        username: 'natS',
+        password: '12bn85oi00'
+    },
+    {
+        name: 'Julia',
+        username: 'housewife_228',
+        password: 'i-love-cakes'
+    },
+    {
+        name: 'Karen',
+        username: 'annoy_woman',
+        password: '1qaz3edc5tgb'
+    }
+];
+
+const field = 'password';
+
+console.log(users[0][field]);
+console.log(users[1][field]);
+console.log(users[2][field]);
+console.log(users[3][field]);
+console.log(users[4][field]);
+console.log(users[5][field]);
+console.log(users[6][field]);
+console.log(users[7][field]);
+console.log(users[8][field]);
+console.log(users[9][field]);
