76f411cfe69b7500a9644399bfa74348c21929a8
AndriiBielich <belichandrey89@gmail.com>
2025-08-04 16:56:03 +0300

    feat: Complete Task 8

---
+++ homeworks/homework_2/task_8/scripts/main.js
@@ -0,0 +1,11 @@
+const day = 17;
+
+if (day >= 1 && day <= 10) {
+    console.log('Перша декада');
+} else if (day >= 11 && day <= 20) {
+    console.log('Друга декада');
+} else if (day >= 21 && day <= 31) {
+    console.log('Третя декада');
+} else {
+    console.log('Invalid data');
+}
