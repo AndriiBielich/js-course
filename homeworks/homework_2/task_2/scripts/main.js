8636d001abd015382ed51f7f619c9cac751e92b7
AndriiBielich <belichandrey89@gmail.com>
2025-08-04 15:58:33 +0300

    feat: Complete Task 2

---
+++ homeworks/homework_2/task_2/scripts/main.js
@@ -0,0 +1,3 @@
+const book1 = { title: 'Algebra 10', pageCount: 255, genre: 'Education' };
+const book2 = { title: 'Century Blizzard', pageCount: 1526, genre: 'Mystery' };
+const book3 = { title: 'Alphabet', pageCount: 30, genre: 'For kids' };
