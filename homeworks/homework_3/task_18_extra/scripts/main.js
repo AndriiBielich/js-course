139fd43a4c8e5f83a2105e81c98b8daf542cc6a0
AndriiBielich <belichandrey89@gmail.com>
2025-08-05 17:07:02 +0300

    feat: Complete Task 18

---
+++ homeworks/homework_3/task_18_extra/scripts/main.js
@@ -0,0 +1,16 @@
+const arr = [];
+
+arr[0] = 1;
+arr[1] = 'hello';
+arr[2] = 222;
+arr[3] = true;
+arr[4] = ['a', 'b', 'c'];
+arr[5] = {};
+arr[6] = -9;
+arr[7] = false;
+arr[8] = 44.5;
+arr[9] = {a: 5, b: 'str'};
+
+for (const item of arr) {
+    console.log(item);
+}
