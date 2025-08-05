4505faddab221fdc1624890dbfe53ef8939d8257
AndriiBielich <belichandrey89@gmail.com>
2025-08-05 14:24:51 +0300

    feat: Complete Task 1

---
+++ homeworks/homework_3/task_1/scripts/main.js
@@ -0,0 +1,18 @@
+const phrases = [
+    "Good morning",
+    "Be yourself",
+    "Keep going",
+    "Stay strong",
+    "Try again",
+    "Never quit",
+    "Be kind",
+    "Stay focused",
+    "Think big",
+    "Dream more"
+];
+
+for (let i = 0; i < phrases.length; i++) {
+    document.write(
+        `<div>${phrases[i]}</div>`
+    );
+}
