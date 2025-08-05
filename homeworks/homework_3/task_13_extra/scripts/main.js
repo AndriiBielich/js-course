d4755236013c05baeddbf754f318c8b1c96b5f16
AndriiBielich <belichandrey89@gmail.com>
2025-08-05 16:49:16 +0300

    feat: Complete Task 13

---
+++ homeworks/homework_3/task_13_extra/scripts/main.js
@@ -0,0 +1,16 @@
+const sentences = [
+    "I like apples.",
+    "She runs fast.",
+    "We are happy.",
+    "He is tall.",
+    "It is cold.",
+    "You look nice.",
+    "They play chess.",
+    "I see a cat.",
+    "We love pizza.",
+    "Birds can fly."
+];
+
+for (const sentence of sentences) {
+    console.log(sentence);
+}
