2873661151d6c73948672e7afbeeb36e875a578a
AndriiBielich <belichandrey89@gmail.com>
2025-08-05 17:20:41 +0300

    feat: Complete Task 23

---
+++ homeworks/homework_3/task_23_extra/scripts/main.js
@@ -0,0 +1,8 @@
+const MAX_STEPS = 100;
+
+for (let i = 0; i <= MAX_STEPS; i++) {
+    if (i % 2 !== 0) {
+        console.log(`Current step - ${i}`);
+        document.write(`<h2>Current step - ${i}`);
+    }
+}
