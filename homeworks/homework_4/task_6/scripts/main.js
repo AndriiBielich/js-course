ddb6ae701e8ee9e7bf5054e9cf032655387455bb
AndriiBielich <belichandrey89@gmail.com>
2025-08-06 16:59:22 +0300

    feat: Complete Task 6

---
+++ homeworks/homework_4/task_6/scripts/main.js
@@ -0,0 +1,10 @@
+function createList(text) {
+    const LIST_SIZE = 3;
+    document.write('<ul>');
+
+    for (let i = 0; i < LIST_SIZE; i++) {
+        document.write(`<li>${text}</li>`);
+    }
+
+    document.write('</ul>');
+}
