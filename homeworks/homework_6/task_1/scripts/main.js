f3f61823c543c3907c092ecdfc56bafcb3c49f12
AndriiBielich <belichandrey89@gmail.com>
2025-08-08 16:26:20 +0300

    feat: Complete Task 1

---
+++ homeworks/homework_6/task_1/scripts/main.js
@@ -0,0 +1,3 @@
+const calculateStringLength = (str) => console.log(`String: "${str}" has length - ${str.length}`);
+const strings = ['hello world', 'lorem ipsum', 'javascript is cool'];
+strings.forEach((string) => calculateStringLength(string));
