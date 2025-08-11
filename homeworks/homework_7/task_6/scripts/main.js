5a4bee04b6d2077825b84e28391fe009d75a0dba
AndriiBielich <belichandrey89@gmail.com>
2025-08-11 17:33:19 +0300

    feat: Complete Task 6

---
+++ homeworks/homework_7/task_6/scripts/main.js
@@ -0,0 +1,39 @@
+function Car(model, manufacturer, yearProduction, maxSpeed, engineVolume) {
+    this.model = model;
+    this.manufacturer = manufacturer;
+    this.yearProduction = yearProduction;
+    this.maxSpeed = maxSpeed;
+    this.engineVolume = engineVolume;
+
+    this.drive = function() {
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
+    }
+
+    this.info = function() {
+        const keys = Object.keys(this);
+        console.log(keys);
+        console.log(`
+            model - ${this.model}\n
+            manufacturer - ${this.manufacturer}\n
+            yearProduction - ${this.yearProduction}\n
+            maxSpeed - ${this.maxSpeed}\n
+            engineVolume - ${this.engineVolume}
+            ${this.driver ? `\ndriver - {name: ${this.driver.name}, age: ${this.driver.age}}` : ''}
+        `);
+    }
+
+    this.increaseMaxSpeed = function(newSpeed) {
+        this.speed = newSpeed > this.maxSpeed ? newSpeed : this.maxSpeed;
+    }
+
+    this.changeYear = function(newYear) {
+        this.yearProduction = newYear;
+    }
+
+    this.addDriver = function(driver) {
+        this.driver = {
+            name: driver.name,
+            age: driver.age
+        }
+    }
+}
