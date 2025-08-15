68ab3944c39d44fac7347ea23a38fc0b001c22d0
AndriiBielich <belichandrey89@gmail.com>
2025-08-15 15:11:26 +0300

    feat: Complete Task 1

---
+++ homeworks/homework_8/task_1/scripts/main.js
@@ -0,0 +1,13 @@
+const coursesAndDurationArray = [
+    { title: 'JavaScript Complex', monthDuration: 5 },
+    { title: 'Java Complex', monthDuration: 6 },
+    { title: 'Python Complex', monthDuration: 6 },
+    { title: 'QA Complex', monthDuration: 4 },
+    { title: 'FullStack', monthDuration: 7 },
+    { title: 'Frontend', monthDuration: 4 }
+];
+
+const coursesAndDurationArrayWithId = coursesAndDurationArray.map((course, index) => ({
+    id: index + 1,
+    ...course
+}));
